8c9c0b053f66f2c5bf301c8de2628d0d5146db9f
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-10-04 15:14:15 +1000

    Update to 1.21.1.

--- forge/src/main/java/net/darkhax/leveltextfix/mixin/MixinEnchantment.java
+++ forge/src/main/java/net/darkhax/leveltextfix/mixin/MixinEnchantment.java
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(Enchantment.class)
 public class MixinEnchantment {
 
-    @ModifyArg(method = "getFullname(I)Lnet/minecraft/network/chat/Component;", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/MutableComponent;append(Lnet/minecraft/network/chat/Component;)Lnet/minecraft/network/chat/MutableComponent;"), index = 0)
-    private Component replaceLevelComponent(Component original) {
+    @ModifyArg(method = "getFullname", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/MutableComponent;append(Lnet/minecraft/network/chat/Component;)Lnet/minecraft/network/chat/MutableComponent;"), index = 0)
+    private static Component replaceLevelComponent(Component original) {
 
         if (LevelTextFixCommon.config != null && LevelTextFixCommon.config.replaceRomanNumerals && original.getContents() instanceof TranslatableContents translatable && translatable.getKey().startsWith("enchantment.level.")) {
 
